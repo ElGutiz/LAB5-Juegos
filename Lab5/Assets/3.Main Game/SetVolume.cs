4f36b17407b9f90ef5da0e1365f44fba6e38ec44
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2020-04-28 19:27:44 -0600

    Slider configurations

--- Lab5/Assets/3.Main Game/SetVolume.cs
+++ Lab5/Assets/3.Main Game/SetVolume.cs
@@ -8,7 +8,7 @@ public class SetVolume : MonoBehaviour
     public AudioMixer mixer;
     public void SetLevel (float sliderValue)
     {
-        mixer.SetFloat("MusicValue", Mathf.Log10 (sliderValue) * 20);
+        mixer.SetFloat("MusicVol", Mathf.Log10 (sliderValue) * 20);
     }
 
 }
